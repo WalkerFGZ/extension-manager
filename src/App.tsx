1039c2b5b0a6f0e302ff2b80ca809ef9d54579ae
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-03-21 15:05:18 -0500

    initial files and config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="bg-neutral-900 text-neutral-100 font-sans">
+      Whereas recognition of the inherent dignity
+    </h1>
+  );
+}
+
+export default App;
